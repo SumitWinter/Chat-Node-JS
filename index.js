659dc3c59abc14c0047d33803717c998f69ec224
Sumit Sagar <58357431+SumitWinter@users.noreply.github.com>
2021-07-01 17:55:23 +0530

    Index.js
    Index.js : Backend handler file

---
+++ index.js
@@ -0,0 +1,53 @@
+const express = require("express");
+const socket = require("socket.io");
+
+// App setup
+const PORT = process.env.PORT || 3000;
+const app = express();
+const server = app.listen(PORT, function () {
+    console.log(`http://localhost:${PORT}`);
+});
+
+
+app.use(express.urlencoded());
+// Static files
+app.use(express.static("public"))
+
+const fs = require("fs");
+const Home = fs.readFileSync('Public/index.html');
+
+// Socket setup
+
+app.get("/", (req, res) => {
+    res.status(200).end(Home);
+    const io = socket(server);
+
+    const activeUsers = new Set();
+
+    io.on("connection", function (socket) {
+        console.log("Made socket connection");
+
+        socket.on("new user", function (data) {
+            socket.userId = data;
+            activeUsers.add(data);
+            io.emit("new user", [...activeUsers]);
+        });
+
+        socket.on("disconnect", () => {
+            activeUsers.delete(socket.userId);
+            io.emit("user disconnected", socket.userId);
+        });
+
+        socket.on("chat message", function (data) {
+            io.emit("chat message", data);
+        });
+
+        socket.on("typing", function (data) {
+            socket.broadcast.emit("typing", data);
+        });
+
+        socket.on("sent", function (data) {
+            socket.broadcast.emit("sent", data);
+        });
+    });
+});
